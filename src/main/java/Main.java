54fd44cd49eaca7474ce76643d73fd0a46fbcfb3
Joe Kutner <jpkutner@gmail.com>
2015-07-30 15:40:55 -0500

    Removed local changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,42 +32,9 @@ public class Main {
           )
           .handlers(c -> {
             c
-              .get("index.html", ctx -> {
-                ctx.redirect(301, "/");
-              })
               .get(ctx -> ctx.render(groovyTemplate("index.html")))
               .get("hello", ctx -> {
-                RelativisticModel.select();
-
-                String energy = System.getenv("ENERGY");
-
-                Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
-                ctx.render("E=mc^2: " + energy + " = " + m.toString());
-              })
-              .get("db", ctx -> {
-                Connection connection = null;
-                Map<String, Object> attributes = new HashMap<>();
-                try {
-                  connection = DatabaseUrl.extract(true).getConnection();
-
-                  Statement stmt = connection.createStatement();
-                  stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-                  stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-                  ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-                  ArrayList<String> output = new ArrayList<String>();
-                  while (rs.next()) {
-                    output.add( "Read from DB: " + rs.getTimestamp("tick"));
-                  }
-
-                  attributes.put("results", output);
-                  ctx.render(groovyTemplate(attributes, "db.html"));
-                } catch (Exception e) {
-                  attributes.put("message", "There was an error: " + e);
-                  ctx.render(groovyTemplate(attributes, "error.html"));
-                } finally {
-                  if (connection != null) try{connection.close();} catch(SQLException e){}
-                }
+                ctx.render("Hello!");
               })
               .assets("public");
           });
