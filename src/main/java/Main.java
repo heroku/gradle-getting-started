0108edcd5919c41eccb3c2a04b2df611b42cd1bf
Joe Kutner <jpkutner@gmail.com>
2015-07-30 15:48:52 -0500

    Sync with master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,10 @@ import ratpack.server.ServerConfig;
 import static ratpack.groovy.Groovy.groovyTemplate;
 import static ratpack.groovy.Groovy.ratpack;
 
+import java.util.*;
+import java.sql.*;
+import com.heroku.sdk.jdbc.DatabaseUrl;
+
 import static javax.measure.unit.SI.KILOGRAM;
 import javax.measure.quantity.Mass;
 import org.jscience.physics.model.RelativisticModel;
@@ -36,6 +40,31 @@ public class Main {
                 RelativisticModel.select();
                 Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
                 ctx.render("E=mc^2: 12 GeV = " + m.toString());
+              })
+              .get("db", ctx -> {
+                Connection connection = null;
+                Map<String, Object> attributes = new HashMap<>();
+                try {
+                  connection = DatabaseUrl.extract(true).getConnection();
+
+                  Statement stmt = connection.createStatement();
+                  stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+                  stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+                  ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+                  ArrayList<String> output = new ArrayList<String>();
+                  while (rs.next()) {
+                    output.add( "Read from DB: " + rs.getTimestamp("tick"));
+                  }
+
+                  attributes.put("results", output);
+                  ctx.render(groovyTemplate(attributes, "db.html"));
+                } catch (Exception e) {
+                  attributes.put("message", "There was an error: " + e);
+                  ctx.render(groovyTemplate(attributes, "error.html"));
+                } finally {
+                  if (connection != null) try{connection.close();} catch(SQLException e){}
+                }
               })
               .assets("public");
           });
