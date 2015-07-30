02aa6f9063f800106e26062da7af85a047404bd5
Joe Kutner <jpkutner@gmail.com>
2015-07-29 19:27:15 -0500

    Added templates

--- src/main/java/my/app/Main.java
+++ src/main/java/my/app/Main.java
@@ -1,14 +1,37 @@
 package my.app;
 
 import ratpack.server.RatpackServer;
+import ratpack.groovy.template.TextTemplateModule;
+import ratpack.guice.Guice;
+import ratpack.server.RatpackServer;
+import ratpack.server.ServerConfig;
+import static ratpack.groovy.Groovy.groovyTemplate;
+import static ratpack.groovy.Groovy.ratpack;
 
 public class Main {
- public static void main(String... args) throws Exception {
-   RatpackServer.start(server -> server
-     .handlers(chain -> chain
-       .get(ctx -> ctx.render("Hello World!"))
-       .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
-     )
-   );
- }
+  public static void main(String... args) throws Exception {
+    RatpackServer.start(b -> {
+        ServerConfig serverConfig = ServerConfig.findBaseDir()
+          .env()
+          .build();
+        b
+          .serverConfig(serverConfig)
+          .registry(
+            Guice.registry(s -> s
+                .module(TextTemplateModule.class, conf ->
+                    conf.setStaticallyCompile(true)
+                )
+            )
+          )
+          .handlers(c -> {
+            c
+              .get("index.html", ctx -> {
+                ctx.redirect(301, "/");
+              })
+              .get(ctx -> ctx.render(groovyTemplate("index.html")))
+              .assets("public");
+          });
+      }
+    );
+  }
 }
