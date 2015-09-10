e0a2467d1f8e758b59cb483ae7ad805b44c99273
rhart <russellxhart@googlemail.com>
2015-09-10 21:59:52 +0100

    Upgrade to rc3 Do blocking I/O off the main thread

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,68 +1,69 @@
+import ratpack.exec.Blocking;
+import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 import ratpack.groovy.template.TextTemplateModule;
 import ratpack.guice.Guice;
-import ratpack.server.RatpackServer;
-import ratpack.server.ServerConfig;
+
 import static ratpack.groovy.Groovy.groovyTemplate;
-import static ratpack.groovy.Groovy.ratpack;
 
 import java.util.*;
 import java.sql.*;
+
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
   public static void main(String... args) throws Exception {
-    RatpackServer.start(b -> {
-        ServerConfig serverConfig = ServerConfig.findBaseDir()
-          .env()
-          .build();
-        b
-          .serverConfig(serverConfig)
-          .registry(
-            Guice.registry(s -> s
-                .module(TextTemplateModule.class, conf ->
-                    conf.setStaticallyCompile(true)
-                )
-            )
-          )
-          .handlers(c -> {
-            c
-              .get("index.html", ctx -> {
-                ctx.redirect(301, "/");
-              })
-              .get(ctx -> ctx.render(groovyTemplate("index.html")))
-              .get("hello", ctx -> {
-                ctx.render("Hello!");
-              })
-              .get("db", ctx -> {
-                Connection connection = null;
-                Map<String, Object> attributes = new HashMap<>();
-                try {
-                  boolean local = !"cedar-14".equals(System.getenv("STACK"));
-                  connection = DatabaseUrl.extract(local).getConnection();
+    RatpackServer.start(s -> s
+        .serverConfig(c -> c
+          .baseDir(BaseDir.find())
+          .env())
 
+        .registry(Guice.registry(b -> b
+          .module(TextTemplateModule.class, conf -> conf.setStaticallyCompile(true))))
+
+        .handlers(chain -> chain
+            .get(ctx -> ctx.render(groovyTemplate("index.html")))
+
+            .get("hello", ctx -> {
+              ctx.render("Hello!");
+            })
+
+            .get("db", ctx -> {
+              boolean local = !"cedar-14".equals(System.getenv("STACK"));
+
+              Blocking.get(() -> {
+                Connection connection = null;
+
+                try {
+                  connection = DatabaseUrl.extract(local).getConnection();
                   Statement stmt = connection.createStatement();
                   stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
                   stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-                  ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-                  ArrayList<String> output = new ArrayList<String>();
-                  while (rs.next()) {
-                    output.add( "Read from DB: " + rs.getTimestamp("tick"));
-                  }
-
-                  attributes.put("results", output);
-                  ctx.render(groovyTemplate(attributes, "db.html"));
-                } catch (Exception e) {
-                  attributes.put("message", "There was an error: " + e);
-                  ctx.render(groovyTemplate(attributes, "error.html"));
+                  return stmt.executeQuery("SELECT tick FROM ticks");
                 } finally {
-                  if (connection != null) try{connection.close();} catch(SQLException e){}
+                  if (connection != null) try {
+                    connection.close();
+                  } catch (SQLException e) {
+                  }
                 }
-              })
-              .assets("public");
-          });
-      }
+              }).onError(throwable -> {
+                Map<String, Object> attributes = new HashMap<>();
+                attributes.put("message", "There was an error: " + throwable);
+                ctx.render(groovyTemplate(attributes, "error.html"));
+              }).then(rs -> {
+                ArrayList<String> output = new ArrayList<>();
+                while (rs.next()) {
+                  output.add("Read from DB: " + rs.getTimestamp("tick"));
+                }
+
+                Map<String, Object> attributes = new HashMap<>();
+                attributes.put("results", output);
+                ctx.render(groovyTemplate(attributes, "db.html"));
+              });
+            })
+
+            .files(f -> f.dir("public"))
+        )
     );
   }
 }
