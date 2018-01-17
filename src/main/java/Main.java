56477ea9027fcdde4a272c1af99381080f308ab1
Joe Kutner <jpkutner@gmail.com>
2018-01-17 16:28:39 -0600

    Upgrade Ratpack and other dependencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import ratpack.exec.Blocking;
+import ratpack.hikari.HikariModule;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 import ratpack.groovy.template.TextTemplateModule;
@@ -9,7 +10,7 @@ import static ratpack.groovy.Groovy.groovyTemplate;
 import java.util.*;
 import java.sql.*;
 
-import com.heroku.sdk.jdbc.DatabaseUrl;
+import javax.sql.DataSource;
 
 public class Main {
   public static void main(String... args) throws Exception {
@@ -18,8 +19,15 @@ public class Main {
           .baseDir(BaseDir.find())
           .env())
 
-        .registry(Guice.registry(b -> b
-          .module(TextTemplateModule.class, conf -> conf.setStaticallyCompile(true))))
+        .registry(Guice.registry(b -> {
+          if (System.getenv("JDBC_DATABASE_URL") != null) {
+            b.module(HikariModule.class, conf -> {
+              conf.addDataSourceProperty("URL", System.getenv("JDBC_DATABASE_URL"));
+              conf.setDataSourceClassName("org.postgresql.ds.PGSimpleDataSource");
+            });
+          }
+          b.module(TextTemplateModule.class, conf -> conf.setStaticallyCompile(true));
+        }))
 
         .handlers(chain -> chain
             .get(ctx -> ctx.render(groovyTemplate("index.html")))
@@ -29,22 +37,14 @@ public class Main {
             })
 
             .get("db", ctx -> {
-              boolean local = !"cedar-14".equals(System.getenv("STACK"));
+              boolean local = !"heroku-16".equals(System.getenv("STACK"));
 
               Blocking.get(() -> {
-                Connection connection = null;
-
-                try {
-                  connection = DatabaseUrl.extract(local).getConnection();
+                try (Connection connection = ctx.get(DataSource.class).getConnection()) {
                   Statement stmt = connection.createStatement();
                   stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
                   stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
                   return stmt.executeQuery("SELECT tick FROM ticks");
-                } finally {
-                  if (connection != null) try {
-                    connection.close();
-                  } catch (SQLException e) {
-                  }
                 }
               }).onError(throwable -> {
                 Map<String, Object> attributes = new HashMap<>();
