4d261ffe1e92e5b1d87972a44292f54456389d2d
Joe Kutner <jpkutner@gmail.com>
2019-02-06 15:22:45 -0600

    Convert to Spring Boot

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: build/install/gradle-getting-started/bin/gradle-getting-started
---
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -0,0 +1,14 @@
+package com.example.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuApplication.class, args);
+	}
+
+}
+
