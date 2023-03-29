a094a6155dbe70e0f8bc1217812d43bf5dce7f88
Manuel Fuchs <manuel.fuchs@salesforce.com>
2023-03-29 12:05:25 +0200

    Add convert endpoint

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.jscience.physics.amount.Amount;
+import org.jscience.physics.model.RelativisticModel;
+import javax.measure.unit.SI;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -46,6 +49,15 @@ public class GettingStartedApplication {
             model.put("message", t.getMessage());
             return "error";
         }
+    }
+
+    @GetMapping("/convert")
+    String convert(Map<String, Object> model) {
+        RelativisticModel.select();
+        var energy = Amount.valueOf("12 GeV");
+
+        model.put("result", "E=mc^2: " + energy + " = " + energy.to(SI.KILOGRAM));
+        return "convert";
     }
 
     public static void main(String[] args) {
